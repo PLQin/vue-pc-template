228472d1c1835822aa7e630f1a5d43d7ff5bc4ee
raymond <599964870@qq.com>
2020-08-28 19:55:52 +0800

    2020-08-28 19:55:51

--- bash/commit.sh
+++ bash/commit.sh
@@ -3,10 +3,10 @@
 # Window only
 # Win使用命令 start [name].sh 即可运行此脚本，或双击脚本亦可
 
-date +"%Y-%m-%d %H-%M-%S"" ------ git add ."
+date +"%Y-%m-%d %H:%M:%S"" ------ git add ."
 git add ../
 
-date +"%Y-%m-%d %H-%M-%S"" ------ git commit"
+date +"%Y-%m-%d %H:%M:%S"" ------ git commit"
 echo -n "请填写commit（可空）"
 read remarks
 if [ ! -n "$remarks" ];then
@@ -14,10 +14,10 @@ if [ ! -n "$remarks" ];then
 fi
 git commit -m "$remarks"
 
-date +"%Y-%m-%d %H-%M-%S"" ------ git pull"
+date +"%Y-%m-%d %H:%M:%S"" ------ git pull"
 git pull
 
-date +"%Y-%m-%d %H-%M-%S"" ------ git push"
+date +"%Y-%m-%d %H:%M:%S"" ------ 请自行执行 git push"
 # git push
 
 exec /bin/bash
--- bash/pull-r.sh
+++ bash/pull-r.sh
@@ -3,13 +3,13 @@
 # Window only
 # Win使用命令 start [name].sh 即可运行此脚本，或双击脚本亦可
 
-date +"%Y-%m-%d %H-%M-%S"" ------ git stash"
+date +"%Y-%m-%d %H:%M:%S"" ------ git stash"
 git stash
 
-date +"%Y-%m-%d %H-%M-%S"" ------ git pull -r"
+date +"%Y-%m-%d %H:%M:%S"" ------ git pull -r"
 git pull -r
 
-date +"%Y-%m-%d %H-%M-%S"" ------ git stash pop"
+date +"%Y-%m-%d %H:%M:%S"" ------ git stash pop"
 git stash pop
 
 exec /bin/bash
